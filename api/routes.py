c6c08751c6e3da3917f8bad484b63939b34d6621
Dhruv Suthar <dhruv8823@gmail.com>
2024-02-18 14:49:10 +0530

    init commit

--- api/routes.py
+++ api/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 from typing import List
-from utils import parse_log_entry, generate_s3_key, upload_logs_to_s3, round_to_nearest_hour, retrive_logs_from_s3_for_candidate
+from utils import parse_log_entry, generate_s3_key, upload_log_to_s3, round_to_nearest_hour, retrive_logs_from_s3_for_candidate
 from collections import defaultdict
 router = APIRouter()
 
@@ -21,23 +21,21 @@ async def ingest_logs(logs:List[str]):
         for key, count in log_counts.items():
             rounded_timestamp, service, log_level, log_message = key.split('_')
             s3_key = generate_s3_key(rounded_timestamp, service, log_level)
-            if upload_logs_to_s3(log_message,count,s3_key):
+            if upload_log_to_s3(log_message,count,s3_key):
                 print(f"Log ingested successfully: {s3_key}")
             else:
                 print(f"Log not ingested: {s3_key}")
 
         return JSONResponse(content={'message': 'Logs ingested successfully'}, status_code=200)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=e.status_code, detail=str(e.detail))
  
-
-
 @router.get('/top-count')
 async def top_count():
     try:
         logs  = retrive_logs_from_s3_for_candidate()
         return JSONResponse(content={'message': logs}, status_code=200)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=e.status_code, detail=str(e.detail))
     
     
--- utils.py
+++ utils.py
@@ -12,8 +12,21 @@ aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 S3_BUCKET = 'mw-code-tester'
 AWS_REGION =  'ap-south-1'
 
-CANDIDATE_NAME = "test410"
+CANDIDATE_NAME = "DHRUV-SUTHAR"
 
+def handle_s3_exception(e):
+    error_code = e.response['Error']['Code']
+    if error_code == 'NoSuchKey':
+        print(f"The key does not exist in the S3 bucket.")
+        return ""
+    elif error_code == 'NoSuchBucket':
+        raise HTTPException(status_code=404, detail=str(e))
+    elif error_code == 'AccessDenied':
+        raise HTTPException(status_code=403, detail=str(e))
+    elif error_code in ['InvalidAccessKeyId', 'SignatureDoesNotMatch']:
+        raise HTTPException(status_code=400, detail=str(e))
+    else:
+        raise HTTPException(status_code=500, detail=str(e))
 
 def round_to_nearest_hour(timestamp_str):
     timestamp = datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%SZ")
@@ -24,14 +37,13 @@ def round_to_nearest_hour(timestamp_str):
     formatted_timestamp = rounded_and_added_hour_timestamp.strftime("%H:%M:%S")
     return f"{rounded_timestamp.isoformat()}-{formatted_timestamp}"
 
-
 def parse_log_entry(log_entry):
     timestamp_str, log_level, service, log_message = log_entry.split(' ', 3)
     service = service.strip('[]')
     return timestamp_str, service, log_level, log_message.strip()
 
 def generate_s3_key(timestamp, service, log_level):
-    key = f"mw-code-tester/{CANDIDATE_NAME}/{timestamp}/{service}/{log_level}/summary.log"
+    key = f"{CANDIDATE_NAME}/{timestamp}/{service}/{log_level}/summary.log"
     return key
 
 def retrive_logs_from_s3(s3,key):
@@ -40,26 +52,23 @@ def retrive_logs_from_s3(s3,key):
         content = response['Body'].read().decode('utf-8')
         return content
     except Exception as e:
-        if e.response['Error']['Code'] == 'NoSuchKey':
-            print(f"The key '{key}' does not exist in the S3 bucket.")
-        else:
-            print(f"An error occurred: {e}")
-        return ""              
+        return handle_s3_exception(e)
 
-def upload_logs_to_s3(log_message,log_count,key):
+             
+def upload_log_to_s3(log_message,log_count,key):
     try:
         existing_content = ""
         s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,region_name=AWS_REGION)
         existing_content = retrive_logs_from_s3(s3,key)
-        updated_content = log_message
+        updated_content = ""
         # Append new content to existing content
         if existing_content != "":
                 search_line = next((line for line in existing_content.split('\n') if log_message in line), None)
                 existing_counter_value = log_count
                 if search_line:
+                    # extracting digits at the beginning for particular line
                     match = re.match(r"(\d+)", search_line)
                     if match:
-                        # Extract existing counter value from the line
                         existing_counter_value = int(match.group(1))
 
                     # Increment the counter value by 1
@@ -70,71 +79,63 @@ def upload_logs_to_s3(log_message,log_count,key):
                 else:
                     log_message = f"{existing_counter_value} - {log_message}"
                     updated_content = existing_content + "\n" +  log_message 
+        else:
+            # add message to new file   
+            log_message = f"{log_count} - {log_message}"
+            updated_content = log_message
 
         response = s3.put_object(Body=updated_content, Bucket=S3_BUCKET, Key=key)
 
         if response.get('ETag') != '':
              return True
-
         return False
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
+        return handle_s3_exception(e)
 
 def retrive_logs_from_s3_for_candidate():
     try:
         s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,region_name=AWS_REGION)
-
         # List all objects in the S3 bucket for the candidate
-        objects = s3.list_objects_v2(Bucket=S3_BUCKET, Prefix=f'mw-code-tester/{CANDIDATE_NAME}/')
+        objects = s3.list_objects_v2(Bucket=S3_BUCKET, Prefix=f'{CANDIDATE_NAME}/')
 
         all_logs = []
-        print(objects.get('Contents', []))
         for obj in objects.get('Contents', []):
                     key = obj['Key']
-
                     # Extract timestamp from the key
-                    timestamp, service, level_type = key.split('/')[2:-1]
-
+                    timestamp, service, level_type = key.split('/')[1:-1]
                     # Read logs from the object
                     logs  = retrive_logs_from_s3(s3,key).split('\n')
                     for log in logs:
                         all_logs.append({
                             'timestamp': timestamp,
                             'service': service,
-                            'level_type': level_type,
+                            'severity': level_type,
                             'log': log
                         })
         
-        return get_top_log(all_logs)
+        return get_top_log(all_logs) if all_logs else f'No logs found for this candidate: {CANDIDATE_NAME}'
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
+        return handle_s3_exception(e)
+         
 def get_top_log(all_logs):
-     current_counter = 0
-     count = 0
-     top_error = ""
-     service = ""
-     severity = ""
-     print(all_logs)
-     for log in all_logs:
-          match = re.match(r"(\d+)", log['log'])
-          if match:
-               if current_counter < int(match.group(1)):
-                        current_counter = int(match.group(1))
-                        count = current_counter
-                        prefix = f"{str(match.group(1))} - "
-                        top_error = log['log'][len(prefix):]
-                        service = log['service']
-                        severity = log['level_type']
-    
-     return {
-          'top_error': top_error,
-          'count': count,
-          'service': service,
-          'severity': severity
-     }
-                    
+    # Find the log with the highest count by extracting digits at the beginning of particular log  
+    max_log = max(all_logs, key=lambda log: int(re.match(r"(\d+)", log['log']).group(1)), default=None)
 
-                
+    # Extract the top error, count, service, and severity type from the max log
+    if max_log:
+        match = re.match(r"(\d+)", max_log['log'])
+        count = int(match.group(1))
+        prefix = f"{str(count)} - "
+        top_error = max_log['log'][len(prefix):]
+        service = max_log['service']
+        severity = max_log['severity']
+
+        return {
+            'top_error': top_error,
+            'count': count,
+            'service': service,
+            'severity': severity
+        }
+    else:
+         return {}
+                    
